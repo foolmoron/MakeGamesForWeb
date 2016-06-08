62b496696361b720c4196283301f2efa8177a928
Momin Khan <foolmoron@gmail.com>
2016-06-07 22:53:38 -0400

    Demo stuff

---
+++ unity/Assets/Rotator.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour {
+
+    [Range(0, 720)]
+    public float RotationSpeed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(0, 0, RotationSpeed * Time.deltaTime);
+	}
+}
