db16623b1ccbcf72f47330c244b246de021cb71a
Andy Bowers <andrewblairbowers@gmail.com>
2016-06-08 12:07:53 -0400

    basic GameScene setup

--- spritekit/MoManPlayground.playground/Contents.swift
+++ spritekit/MoManPlayground.playground/Contents.swift
@@ -1,5 +1,38 @@
 //: Playground - noun: a place where people can play
 
-import UIKit
+import SpriteKit
+import XCPlayground
 
-var str = "Hello, playground"
+struct Constants {
+	static let gameWindow = CGRect(x: 0, y: 0, width: 640, height: 640)
+}
+
+class GameScene: SKScene {
+	override init(size: CGSize) {
+		super.init(size: size)
+		
+		setup()
+		
+	}
+	
+	required init?(coder aDecoder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+	
+	func setup() {
+		setupBackground()
+		
+	}
+	
+	func setupBackground() {
+		backgroundColor = UIColor.magentaColor()
+	}
+	
+}
+
+//:- Main
+var scene = GameScene(size: Constants.gameWindow.size)
+let view = SKView(frame: Constants.gameWindow)
+view.presentScene(scene)
+
+XCPlaygroundPage.currentPage.liveView = view
