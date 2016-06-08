974e809d415224673f8028f49d34f1f973862fd1
Andy Bowers <andrewblairbowers@gmail.com>
2016-06-08 12:58:48 -0400

    basic click and randomized target

--- spritekit/MoManPlayground.playground/Contents.swift
+++ spritekit/MoManPlayground.playground/Contents.swift
@@ -4,15 +4,18 @@ import SpriteKit
 import XCPlayground
 
 struct Constants {
-	static let gameWindow = CGRect(x: 0, y: 0, width: 640, height: 640)
+	static let gameWindow = CGRect(x: 0, y: 0, width: 800, height: 600)
+	static let margins = UIEdgeInsets(top: 16, left: 16, bottom: 16, right: 16)
 }
 
 class GameScene: SKScene {
+	
+	var target: SKSpriteNode?
+	
 	override init(size: CGSize) {
 		super.init(size: size)
 		
 		setup()
-		
 	}
 	
 	required init?(coder aDecoder: NSCoder) {
@@ -21,13 +24,52 @@ class GameScene: SKScene {
 	
 	func setup() {
 		setupBackground()
-		
+		setupTarget()
 	}
 	
 	func setupBackground() {
-		backgroundColor = UIColor.magentaColor()
+		let background = SKSpriteNode(imageNamed: "bg")
+		background.anchorPoint = CGPoint.zero
+		addChild(background)
 	}
 	
+	func setupTarget() {
+		target = SKSpriteNode(imageNamed: "target")
+		guard let target = target else {
+			fatalError()
+		}
+		addChild(target)
+		randomizeTargetPosition()
+	}
+	
+	func randomizeTargetPosition() {
+		guard let target = target else {
+			return
+		}
+		let randomXFactor = Double(arc4random() % 100) / 100.0
+		let randomYFactor = Double(arc4random() % 100) / 100.0
+		
+		let randomX = Double(Constants.gameWindow.width) * randomXFactor
+		let randomY = Double(Constants.gameWindow.height) * randomYFactor
+		
+		let minX = Double(Constants.margins.left)
+		let maxX = Double(Constants.gameWindow.width - Constants.margins.right)
+		let minY = Double(Constants.margins.bottom)
+		let maxY = Double(Constants.gameWindow.height - Constants.margins.top)
+		
+		let actualX = max(min(randomX, maxX), minX)
+		let actualY = max(min(randomY, maxY), minY)
+		
+		target.position = CGPoint(x: actualX, y: actualY)
+	}
+	
+	override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+		for touch in touches {
+			if let target = target where target.frame.contains(touch.locationInNode(self)) {
+				randomizeTargetPosition()
+			}
+		}
+	}
 }
 
 //:- Main
